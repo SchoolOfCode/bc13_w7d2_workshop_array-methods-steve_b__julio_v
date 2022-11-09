59577ba5ca6a3ccec27ed35e8fa891813f8356c3
Julio Velezmoro <julio28ve@gmail.com>
2022-11-09 10:09:41 +0000

    Task 6

--- task6.js
+++ task6.js
@@ -16,3 +16,12 @@ const students = [
   "Luna Lovegood",
   "Gregory Goyle",
 ];
+// 6a. Using the .sort() method, rearrange the brooms (numbers) in the broomCupboard array so that they are in ASCENDING order, with broomstick 1 at index 0 and broomstick 10 and index 9.
+broomCupboard.sort((a,b)=>a-b);
+console.log('Sort broomCupboard',broomCupboard);
+// 6b. Using the .sort() method and the shelf property of each potions object, rearrange the four potions in the 'potions' array so they are in DESCENDING order of their shelves, with the 'draught of living death' at index 0 and the 'pepperup potion' at index 3.
+potions.sort((a,b)=>b.shelf-a.shelf);
+console.log('Sort potions',potions);
+//6c. Sort the names in the array in alphebetical order according to their last name.
+
+
