2b8318a818f96a4d316a175cb9c0512a54a4f8f7
Steve Beecheno <iamhacktinium@gmail.com>
2022-11-09 19:47:30 +0000

    Up to 6a complete

--- task6.js
+++ task6.js
@@ -1,18 +1,19 @@
+// 6a.
+
 const broomCupboard = [2, 4, 1, 3, 7, 5, 8, 6, 10, 9, 0];
+
+const students = ["Cedric Diggory", "Padma Patil", "Lucius Malfoy", "Seamus Finnigan", "Lavender Brown", "Luna Lovegood", "Gregory Goyle"];
+
+broomCupboard.splice(broomCupboard.length - 1, 1);
+broomCupboard.sort((a, b) => a - b);
+console.log(broomCupboard);
+
+
+// 6b.
 
 const potions = [
   { name: "polyjuice potion", shelf: 2 },
   { name: "draught of living death", shelf: 4 },
   { name: "pepperup potion", shelf: 1 },
   { name: "veritaserum", shelf: 3 },
-];
-
-const students = [
-  "Cedric Diggory",
-  "Padma Patil",
-  "Lucius Malfoy",
-  "Seamus Finnigan",
-  "Lavender Brown",
-  "Luna Lovegood",
-  "Gregory Goyle",
 ];
